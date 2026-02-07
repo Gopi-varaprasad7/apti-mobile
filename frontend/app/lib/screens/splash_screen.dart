f9cafa74c9de1111e8beb567481b69e2e8a19e65
Koduri Gopi Varaprasad <kodurigopi98722@gmail.com>
2026-02-07 16:13:09 +0530

    splash screen

---
+++ frontend/app/lib/screens/splash_screen.dart
@@ -0,0 +1,64 @@
+import 'dart:async';
+
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashSScreenState();
+}
+
+class _SplashSScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(milliseconds: 4), () {
+      if (mounted) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const LoginScreen()),
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              'Apti',
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            DefaultTextStyle(
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontSize: 40,
+              ),
+              child: AnimatedTextKit(
+                animatedTexts: [
+                  TypewriterAnimatedText(
+                    'Verve',
+                    speed: const Duration(milliseconds: 300),
+                  ),
+                ],
+                totalRepeatCount: 1,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
