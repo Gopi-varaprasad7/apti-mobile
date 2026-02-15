7273d8f96c2af68c352d686f00696edf6a50675c
Koduri Gopi Varaprasad <kodurigopi98722@gmail.com>
2026-02-15 17:22:54 +0530

    date page

--- frontend/app/lib/screens/habits/date_screen.dart
+++ frontend/app/lib/screens/habits/date_screen.dart
@@ -1,10 +1,72 @@
+import 'package:app/models/habit_model.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class DateScreen extends StatelessWidget {
-  const DateScreen({super.key});
+  final List<Habit> habits;
+
+  const DateScreen({super.key, required this.habits});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Habit Timeline")),
+      body:
+          habits.isEmpty
+              ? const Center(child: Text("No habits created"))
+              : ListView.builder(
+                padding: const EdgeInsets.all(16),
+                itemCount: habits.length,
+                itemBuilder: (context, index) {
+                  final habit = habits[index];
+
+                  return Card(
+                    margin: const EdgeInsets.only(bottom: 15),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            habit.name,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: habit.color,
+                            ),
+                          ),
+
+                          const SizedBox(height: 10),
+
+                          Row(
+                            children: [
+                              const Icon(Icons.play_arrow, size: 18),
+                              const SizedBox(width: 6),
+                              Text(
+                                "Started: ${DateFormat.yMMMd().format(habit.startDate)}",
+                              ),
+                            ],
+                          ),
+
+                          const SizedBox(height: 6),
+
+                          Row(
+                            children: [
+                              const Icon(Icons.stop, size: 18),
+                              const SizedBox(width: 6),
+                              Text(
+                                habit.endDate == null
+                                    ? "End: Ongoing"
+                                    : "Ended: ${DateFormat.yMMMd().format(habit.endDate!)}",
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+    );
   }
 }
