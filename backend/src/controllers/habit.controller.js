039378256abd5562318c5b50ccaba92fa07009a3
Koduri Gopi Varaprasad <kodurigopi98722@gmail.com>
2026-02-23 11:51:58 +0530

    create habit

---
+++ backend/src/controllers/habit.controller.js
@@ -0,0 +1,17 @@
+const db = require('../config/db');
+const {v4:uuidv4} = require('uuid');
+
+exports.createHabit = async (req,res) => {
+    const {name, period, color , userId} = req.body;
+
+    const id = uuidv4();
+
+    const query = "INSERT INTO Habits (id, name, period, color, userId) VALUES (? , ? , ? , ? , ? )";
+    db.query(query,[id,name,period,color,userId], (err,result) => {
+        if(err) return res.status(500).json(err);
+        res.status(201).json({
+            message: "Habit created successfully",
+            habitId: id,
+          });
+    })
+}
